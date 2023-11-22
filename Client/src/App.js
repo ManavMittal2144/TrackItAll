8680ea39a093f49698b7a8259e1bddce28ae1b80
MANAV MITTAL <99489415+ManavMittal2144@users.noreply.github.com>
2023-11-22 10:32:45 +0530

    Add files via upload

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,34 @@
-import { Routes,Route,Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import ToDoList from "./pages/ToDoPage";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<ProtectedRoutes><HomePage/></ProtectedRoutes>}/>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="/To_Do_List" element={<ToDoList />} />
+        <Route path="/Expense_Tracker" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
 }
 export function ProtectedRoutes(props) {
-  if (localStorage.getItem('user')) {
-    return props.children
-  }
-  else {
-    return <Navigate to = "/login" />
+  if (localStorage.getItem("user")) {
+    return props.children;
+  } else {
+    return <Navigate to="/login" />;
   }
 }
 
