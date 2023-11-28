cdb137763bb73af63d279d6c1fc89ec17dc7b253
vaishnavi-agrawall <101472652+vaishnavi-agrawall@users.noreply.github.com>
2023-11-28 20:19:55 +0530

    Add files via upload

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { message } from "antd";
 
@@ -10,14 +10,6 @@ const Home = () => {
     message.success("Logout Successfully");
     navigate("/");
   };
-
-  // Assuming you have a useEffect somewhere in your component
-  useEffect(() => {
-    // Your useEffect code
-
-    // Make sure to include all dependencies, for example:
-
-  }, []); // Include all dependencies here
 
   return (
     <>
@@ -36,9 +28,7 @@ const Home = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link className="navbar-brand" to="/">
-            <button className="btn btn-primary">
-              Home
-              </button>
+              TrackItAll
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -56,7 +46,7 @@ const Home = () => {
 
         <div className="row mt-5">
           {/* First Image and Name */}
-          <div className="col-md-6">
+          <div className="col-md-4">
             <Link to="/Expense_Tracker" className="btn btn-primary">
               <img
                 src="https://cdn.vectorstock.com/i/1000x1000/08/43/expense-tracker-app-rgb-color-icon-vector-39050843.webp"
@@ -73,7 +63,7 @@ const Home = () => {
           </div>
 
           {/* Second Image and Name */}
-          <div className="col-md-6">
+          <div className="col-md-4">
             <Link to="/To_Do_List" className="btn btn-success">
               <img
                 src="https://adniasolutions.com/wp-content/webp-express/webp-images/uploads/2018/05/To-Do-List-Excel-Template-01.png.webp"
@@ -85,6 +75,23 @@ const Home = () => {
             <div className="mt-3">
               <Link to="/To_Do_List" className="btn btn-success">
                 TO-DO-LIST
+              </Link>
+            </div>
+          </div>
+
+          {/* Third image */}
+          <div className="col-md-4">
+            <Link to="/Diaries" className="btn btn-primary">
+              <img
+                src="https://cdn.designbump.com/wp-content/uploads/2011/10/diary-website-layout-photoshop-tutorial-template-photoshop-templates-website-design-graphic-design-web-designer.jpg"
+                alt="Diary"
+                className="img-fluid rounded"
+                style={{ height: "250px", width: "250px" }}
+              />
+            </Link>
+            <div className="mt-3">
+              <Link to="/Diaries" className="btn btn-primary">
+                Diary
               </Link>
             </div>
           </div>
