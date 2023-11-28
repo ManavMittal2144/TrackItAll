3f0f5eb8f9a6a4b00d3ed8da044f8aaf9f5aece4
nick1876 <133684127+nick1876@users.noreply.github.com>
2023-11-28 16:05:58 +0530

    Add files via upload
    diary added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const connectDb = require("./config/connectDb");
@@ -17,11 +18,14 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 //routes
 app.use("/api/v1/users", require("./routes/userRoute"));
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 app.use("/api/v1/To_Do_List", require("./routes/todoRoute"));
+
+app.use("/api/v1/Diaries", require("./routes/diaryRoutes"));
 
 //port
 const PORT = 8080 || process.env.PORT;
